6d67b4bd71418c9f33db05141571d7f80854ea7e
xiehaihong <xinlle.01@163.com>
2023-09-04 13:39:56 +0800

    修改组件名

--- src/components/sl-input/sl-input.vue
+++ src/components/sl-input/sl-input.vue
@@ -1,86 +1,100 @@
 <template>
-  <view :class="[clz.root(), className]" class="flex">
+  <view :class="[clz.root(), className]">
     <!--body-->
-    <view
-      :class="[clz.body(), paddingClz, borderClz, `radius-${radius}`, `bg-${bg}`]"
-      class="flex flex-grow font-title color-content"
-    >
-      <sl-icon v-if="leftIcon.name?.length > 0" v-bind="leftIcon" />
+    <view :class="clz.body()" class="flex">
       <view
-        v-if="prefix || $slots.prefix"
-        :class="[clz.join('body', 'prefix')]"
-        class="flex-shrink mr-xs"
+        :class="[
+          clz.join('body', 'container'),
+          paddingClz,
+          borderClz,
+          `radius-${radius}`,
+          `bg-${bg}`,
+        ]"
+        class="flex flex-grow font-title color-content"
       >
-        <slot name="prefix">
-          <text :class="[clz.join('body', 'prefix', 'text'), prefixClz]">{{ prefix }}</text>
-        </slot>
-      </view>
+        <sl-icon v-if="leftIcon.name?.length > 0" v-bind="leftIcon" />
+        <view
+          v-if="prefix || $slots.prefix"
+          :class="[clz.join('body', 'prefix')]"
+          class="flex-shrink mr-xs"
+        >
+          <slot name="prefix">
+            <text :class="[clz.join('body', 'prefix', 'text'), prefixClz]">{{ prefix }}</text>
+          </slot>
+        </view>
 
-      <view
-        :class="[clz.join('body', 'wrap'), type === 'textarea' ? 'flex-plain' : 'flex']"
-        class="flex-grow"
-      >
-        <textarea
-          v-if="type === 'textarea'"
-          :class="[
-            clz.join('body', 'wrap', 'textarea'),
-            `font-${fontSize}`,
-            `text-${align}`,
-            `color-${disabled ? 'disabled' : color}`,
-          ]"
-          class="flex-grow border-none w100"
-          :value="value"
-          :placeholder="placeholder"
-          :placeholderStyle="placeholderStyle"
-          :disabled="disabled"
-          :maxlength="maxlength"
-          :fixed="fixed"
-          :focus="focus"
-          :confirmType="confirmType"
-          :cursorSpacing="cursorSpacing"
-          :SelectionStart="SelectionStart"
-          :SelectionEnd="SelectionEnd"
-          :style="{ ...Object.assign({ height: height, ...styleObj }) }"
-          @focus="handleFocus"
-          @blur="handleBlur"
-          @input="handleInput"
-          @confirm="$emit('confirm', ($event as any).detail.value)"
-        />
-        <input
-          v-else
-          :class="[
-            clz.join('body', 'wrap', 'input'),
-            `height-${heightSize}`,
-            `font-${fontSize}`,
-            `text-${align}`,
-            `color-${disabled ? 'disabled' : color}`,
-          ]"
-          class="flex-grow border-none"
-          :type="type === 'password' ? 'text' : type"
-          :value="value"
-          :password="type === 'password' && !showPassword"
-          :placeholder="placeholder"
-          :disabled="disabled"
-          :maxlength="maxlength"
-          :focus="focus"
-          :confirmType="confirmType"
-          :cursorSpacing="cursorSpacing"
-          :SelectionStart="SelectionStart"
-          :SelectionEnd="SelectionEnd"
-          :style="styleObj"
-          @focus="handleFocus"
-          @blur="handleBlur"
-          @input="handleInput"
-          @confirm="$emit('confirm', ($event as any).detail.value)"
-        />
-        <view v-if="showClear && focus && value.length > 0" @tap="handleClear">
-          <sl-icon v-bind="clearIcon" />
+        <view
+          :class="[clz.join('body', 'wrap'), type === 'textarea' ? 'flex-plain' : 'flex']"
+          class="flex-grow"
+        >
+          <textarea
+            v-if="type === 'textarea'"
+            :class="[
+              clz.join('body', 'wrap', 'textarea'),
+              `font-${fontSize}`,
+              `text-${align}`,
+              `color-${disabled ? 'disabled' : color}`,
+            ]"
+            class="flex-grow border-none w100"
+            :value="value"
+            :placeholder="placeholder"
+            :placeholderStyle="placeholderStyle"
+            :disabled="disabled"
+            :maxlength="maxlength"
+            :fixed="fixed"
+            :focus="focus"
+            :confirmType="confirmType"
+            :cursorSpacing="cursorSpacing"
+            :SelectionStart="SelectionStart"
+            :SelectionEnd="SelectionEnd"
+            :style="{ ...Object.assign({ height: height, ...styleObj }) }"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @input="handleInput"
+            @confirm="$emit('confirm', ($event as any).detail.value)"
+          />
+          <input
+            v-else
+            :class="[
+              clz.join('body', 'wrap', 'input'),
+              `height-${heightSize}`,
+              `font-${fontSize}`,
+              `text-${align}`,
+              `color-${disabled ? 'disabled' : color}`,
+            ]"
+            class="flex-grow border-none"
+            :type="type === 'password' ? 'text' : type"
+            :value="value"
+            :password="type === 'password' && !showPassword"
+            :placeholder="placeholder"
+            :disabled="disabled"
+            :maxlength="maxlength"
+            :focus="focus"
+            :confirmType="confirmType"
+            :cursorSpacing="cursorSpacing"
+            :SelectionStart="SelectionStart"
+            :SelectionEnd="SelectionEnd"
+            :style="styleObj"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @input="handleInput"
+            @confirm="$emit('confirm', ($event as any).detail.value)"
+          />
+          <view v-if="showClear && focus && value.length > 0" @tap="handleClear">
+            <sl-icon v-bind="clearIcon" />
+          </view>
         </view>
       </view>
+      <!--right-->
+      <view :class="[clz.right()]" class="flex-shrink">
+        <slot name="right" />
+      </view>
     </view>
-    <!--right-->
-    <view :class="[clz.right()]" class="flex-shrink">
-      <slot name="right" />
+    <!--foot-->
+    <!--表单类都要有foot, 用于验证提示那些-->
+    <view :class="[clz.foot()]">
+      <slot name="validate" />
+      <slot name="foot" />
     </view>
   </view>
 </template>
@@ -90,7 +104,7 @@
   import { props } from './_props';
   import { useInput } from './useInput';
 
-  const ComponentName = 'sl-row';
+  const ComponentName = 'sl-input';
   const clz = useClassName(ComponentName);
 
   const _props = defineProps(props);
